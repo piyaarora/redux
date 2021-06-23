72c107aee6c4de09e000ebfd9048184be1b64151
Priya Arora <priya.arora@integrella.com>
2021-06-23 10:11:48 +0530

    changes pushed

---
+++ src/redux/actions/productActions.js
@@ -0,0 +1,23 @@
+import { ActionTypes } from "../constants/actionTypes"
+
+export const setProducts = (products) =>{
+    return{
+        type : ActionTypes.SET_PRODUCT,
+        payload:products,
+    }
+};
+
+export const selectedProduct = (product) =>{
+    return{
+        type: ActionTypes.SELECTED_PRODUCT,
+        payload: product
+    }
+} 
+
+
+export const RemoveSelectedProduct = () =>{
+    return{
+        type: ActionTypes.REMOVE_SELECTED_PRODUCT,
+    
+    }
+} 
---
+++ src/redux/reducers/productReducer.js
@@ -0,0 +1,28 @@
+import { ActionTypes } from "../constants/actionTypes"
+const initialState = {
+    products:[],
+}
+
+export const productReducer = (state = initialState, action) =>{
+    const {type, payload} = action
+    switch(type){
+        case ActionTypes.SET_PRODUCT:
+            return {...state, products:payload};
+        default:
+            return state;
+    }
+} 
+
+
+export const selectedProductReducer = (state = initialState, action) =>{
+    const {type, payload} = action
+    switch(type){
+        case ActionTypes.SELECTED_PRODUCT:
+            return {...state, ...payload};
+        case ActionTypes.REMOVE_SELECTED_PRODUCT:
+            return {};
+        
+        default:
+            return state;
+    }
+} 
